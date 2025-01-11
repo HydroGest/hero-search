7ae30ea7dc29e9e9bd5eb77ad04a8cf2ff209b69
MarkChai <87580250+HydroGest@users.noreply.github.com>
2025-01-11 22:58:29 +0800

    feat: 基础框架

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'hero-search'
 
@@ -7,5 +7,15 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('搜图 <message>')
+  .action((_, message) => {
+    const session = _.session;
+    ctx.logger.info(message);
+    let imgList = h.select(message, "img").map((item) => item.attrs.src);
+    if (!imgList || !imgList.length) {
+        session.send("无效参数，请求操作结束");
+        return;
+    }
+    session.send("搜索原图中...");
+  })
 }
