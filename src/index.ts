21d325206acaaaa6445881e5c0c09869f7675577
MarkChai <87580250+HydroGest@users.noreply.github.com>
2025-01-11 23:30:28 +0800

    feat: 基础功能实现

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,39 @@ export const name = 'hero-search'
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  token: string
+  containUrl: bool
+}
 
-export function apply(ctx: Context) {
+export const Config: Schema<Config> = Schema.object({
+  token: Schema.string().required().description('你 SauceNAO.com 的 API Token'),
+  containUrl: Schema.boolean().default(1),
+})
+
+export function apply(ctx: Context, config: Config) {
+  
+  const sagiri = require("sagiri");
+  
   ctx.command('搜图 <message>')
-  .action((_, message) => {
+  .action(async (_, message) => {
     const session = _.session;
     ctx.logger.info(message);
     let imgList = h.select(message, "img").map((item) => item.attrs.src);
     if (!imgList || !imgList.length) {
-        session.send("无效参数，请求操作结束");
+        await session.send("无效参数，请求操作结束");
         return;
     }
-    session.send("搜索原图中...");
+    await session.send("搜索原图中...");
+    const client = sagiri(config.token);
+    const results = await client(imgList[0]);
+    let msgList: string = "";
+    results.forEach(async (item) => {
+      msgList += `<message>${(config.containUrl?: `链接：${item.url}` : "")}
+作者：${item.authorName}
+网站：${item.site}
+相似度：${item.similarity}</message>`;
+    })
+    await session.send(`<messages forward>${msgList}</messages>`);
   })
 }
