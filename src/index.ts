26ae0f3a2a3e894d96130497cf9c00f5f657f0d8
MarkChai <87580250+HydroGest@users.noreply.github.com>
2025-01-12 17:37:49 +0800

    Lint: 移除多余换行

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,10 @@ export function apply(ctx: Context, config: Config) {
     const results = await client(imgList[0]);
     let msgList: string[] = [];
     results.forEach((item) => {
-      msgList.push(`<message>${config.containUrl ? `链接：${item.url}\n` : ""}作者：${item.authorName}
+      msgList.push(`<message>${config.containUrl ? `链接：${item.url}` : ""}
+作者：${(item.authorName ? item.authorName: "未知")}
 网站：${item.site}
-相似度：${item.similarity}</message>`);
+相似度：${item.similarity}%</message>`);
     });
     await session.send(`<message forward>${msgList.join()}</message>`);
   });
